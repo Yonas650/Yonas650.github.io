0cacd0407a1aa57ade0d24bbcdf4ccb1657942b5
Yonas650 <111200835+Yonas650@users.noreply.github.com>
2024-08-22 01:58:22 +0300

    update

--- pages/about.jsx
+++ pages/about.jsx
@@ -15,18 +15,18 @@ const AboutPage = () => {
 
       <h3>Computer Science Experience</h3> <br/>
 <ol>
-    <li>Summer Research Assistant at <a href='https://aimlab-haptics.com/'>AIM lab, NYUAD</a>, Developed and implemented Graph Neural Networks for EEG-based neurohaptic research, focusing on link prediction and graph classification.</li>
+    <li><span style="color: green;">Summer Research Assistant</span> at <a href='https://aimlab-haptics.com/'>AIM lab, NYUAD</a>, Developed and implemented Graph Neural Networks for EEG-based neurohaptic research, focusing on link prediction and graph classification.</li>
     <br/>
-    <li>Tech Intern at a start-up (Project KUWA): I worked as a full-stack web developer, using MERN stack.</li>
+    <li><span style="color: green;">Tech Intern at a start-up (Project KUWA)</span>: I worked as a full-stack web developer, using MERN stack.</li>
     <br/>
-    <li>AI Peer Mentor at <a href='https://nyuad.nyu.edu/en/admissions/undergraduate/why-nyu-abu-dhabi/nyuad-programs-for-high-school-students/nyuad-design-lab.html'> Design Lab:AI, NYUAD</a>: Mentored high school students from around the world, helping them with AI projects, preparing presentations, and facilitating discussions.</li>
+    <li><span style="color: green;">AI Peer Mentor)</span> at <a href='https://nyuad.nyu.edu/en/admissions/undergraduate/why-nyu-abu-dhabi/nyuad-programs-for-high-school-students/nyuad-design-lab.html'> Design Lab:AI, NYUAD</a>: Mentored high school students from around the world, helping them with AI projects, preparing presentations, and facilitating discussions.</li>
     <br/>
 </ol>   
 <h3>Additional Experience</h3> <br/>
 <ol>
-    <li>NYUAD Art Gallery Assistant - Production and Docent: Managed the front desk, guided tours, and assisted the exhibition design technician with special projects.</li>
+    <li><span style="color: green;">NYUAD Art Gallery Assistant - Production and Docent</span>: Managed the front desk, guided tours, and assisted the exhibition design technician with special projects.</li>
     <br/>
-    <li>Varsity Athletic Events Staff at NYU Athletics: Guiding visiting teams, coaches, opponents, and officials (Primary host responsibility for teams and VIP’S).</li>
+    <li><span style="color: green;">Varsity Athletic Events Staff at NYU Athletics</span>: Guiding visiting teams, coaches, opponents, and officials (Primary host responsibility for teams and VIP’S).</li>
   </ol>  
 
 
